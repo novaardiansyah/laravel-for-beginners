c7db611e082d012f6f1bb956f475abc62f8aea35
novaardiansyah <novaardiansyah78@gmail.com>
2021-10-14 17:23:48 +0700

    Make improvements to the posts list
    * Replacing the list with a card
    * move categories into card images

--- resources/views/blog/posts.blade.php
+++ resources/views/blog/posts.blade.php
@@ -7,10 +7,10 @@
 
       @if ($posts->count())
         <div class="card mb-3">
-          <img src="https://source.unsplash.com/1080x480/?{{ $posts[0]->category->name }}" class="card-img-top"
+          <img src="https://source.unsplash.com/1080x480?{{ $posts[0]->category->name }}" class="card-img-top"
             alt="{{ $posts[0]->category->name }}">
           <div class="card-body text-center">
-            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none">
+            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">
                 {{ $posts[0]->title }}
               </a></h3>
 
@@ -19,6 +19,7 @@
                 href="/categories/{{ $posts[0]->category->slug }}"
                 class="text-decoration-none">{{ $posts[0]->category->name }}</a> <span
                 class="text-lowercase">{{ $posts[0]->created_at->diffForHumans() }}</span></small>
+
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
 
             <a href="/posts/{{ $posts[0]->slug }}" class="btn btn-primary text-decoration-none">
@@ -29,27 +30,36 @@
       @else
         <p class="text-center fs-4">Post not found,</p>
       @endif
-
-      <ul class="list-group">
-        @foreach ($posts as $post)
-          <li class="list-group-item py-3 shadow-sm">
-            <h5 class="mb-0">
-              <a href="/posts/{{ $post->slug }}" class="text-decoration-none">
-                {{ $post->title }}
-              </a>
-            </h5>
-            <small class="d-block mb-3 text-capitalize">By. <a href="/authors/{{ $post->user->username }}"
-                class="text-decoration-none">{{ $post->user->name }}</a> in <a
-                href="/categories/{{ $post->category->slug }}"
-                class="text-decoration-none">{{ $post->category->name }}</a></small>
-
-            <p class="mb-0">{{ $post->excerpt }}</p>
-            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">
-              Read More..
-            </a>
-          </li>
-        @endforeach
-      </ul>
     </div>
+  </div>
+  <div class="row">
+    @foreach ($posts->skip(1) as $post)
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, .7)">
+            <a href="/categories/{{ $post->category->slug }}"
+              class="text-white text-decoration-none">{{ $post->category->name }}</a>
+          </div>
+
+          <img src="https://source.unsplash.com/500x300?{{ $post->category->name }}" class="card-img-top"
+            alt="{{ $post->category->name }}" />
+
+          <div class="card-body">
+            <h5 class="card-title"><a href="/posts/{{ $post->slug }}" class="text-decoration-none">
+                {{ $post->title }}
+              </a></h5>
+
+            <small class="d-block mb-3 text-capitalize text-muted">By. <a
+                href="/authors/{{ $posts[0]->user->username }}"
+                class="text-decoration-none">{{ $posts[0]->user->name }}</a> <span
+                class="text-lowercase">{{ $posts[0]->created_at->diffForHumans() }}</span></small>
+
+            <p class="card-text">{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="btn btn-primary text-decoration-none">Read More</a>
+          </div>
+        </div>
+      </div>
+    @endforeach
   </div>
 @endsection
