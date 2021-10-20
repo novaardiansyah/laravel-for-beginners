0c920c5e589fb6cc7fde570bb641dffd020c67aa
novaardiansyah <novaardiansyah78@gmail.com>
2021-10-20 17:46:20 +0700

    fix controller post
    * make the "Posts" page title dynamic

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
   public function index()
   {
+    $title = "";
+
+    if (request("category")) {
+      $category = Category::firstWhere("slug", request("category"));
+      $title = " in " . $category->name;
+    }
+
+    if (request("authors")) {
+      $authors = User::firstWhere("username", request("authors"));
+      $title = " by " . $authors->name;
+    }
+
     return view('blog.posts', [
-      "title"  => "All Posts",
+      "title"  => "All Posts" . $title,
       "active" => "posts",
       "posts"  => Post::filter(request(['search', 'category', 'authors']))->latest()->get()
     ]);
