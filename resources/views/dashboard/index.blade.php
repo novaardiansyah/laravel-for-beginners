e713112aae8bb6184e4692451e3159914d5b52eb
novaardiansyah <novaardiansyah78@gmail.com>
2021-10-26 22:34:51 +0700

    set up features for login
    * setting up controllers, views, and routes for the dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('container')
+  <div class="row justify-content-center">
+    <div class="col-lg-8">
+      <h3>Welcome to the dashboard page.</h3>
+    </div>
+  </div>
+@endsection
