ee8c852a43e2273c56f6853413d7192b35db530e
novaardiansyah <novaardiansyah78@gmail.com>
2021-11-03 21:47:24 +0700

    set up authorization for admins and regular users
    * added the is_admin field to the user table
    * update gate and middleware with field is_admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
    */
   public function handle(Request $request, Closure $next)
   {
-    if (auth()->guest() || auth()->user()->username !== 'novaardiansyah') {
+    if (auth()->guest() || auth()->user()->is_admin == 0) {
       // ! 403 Forbidden
       abort(403);
     }
